7300c4acedea635e9af9fd41526760c836a3afb9
Herkko Mehtala <75428768+Herkkomehtala@users.noreply.github.com>
2022-11-09 08:18:12 -0800

    Add base-json generator script

--- gen_vulnAD.ps1
+++ gen_vulnAD.ps1
@@ -6,7 +6,13 @@ function CreateADGroup() {
 
     $name = $groupObject.name
     New-ADGroup -name $name -GroupScope Global
+}
 
+function RemoveADGroup(){
+    param([Parameter(Mandatory=$true)] $groupObject)
+
+    $name = $groupObject.name
+    Remove-ADGroup -Identity $name -Confirm:$false
 }
 function CreateADUser(){
     param([Parameter(Mandatory=$true)] $userObject)
@@ -37,6 +43,32 @@ function CreateADUser(){
         }
     }
     
+}
+
+function RemoveADUser(){
+    param([Parameter(Mandatory=$true)] $userObject)
+
+    $name = $userObject.name
+    $firstname, $lastname = $name.Split(" ")
+    $username = ($firstname[0] + $lastname).ToLower()
+    $samAccountName = $username
+
+    Remove-ADUser -Identity $samAccountName
+
+}
+
+function NerfPasswordPolicy(){
+    secedit /export /cfg c:\secpol.cfg
+    (Get-Content C:\secpol.cfg).replace("PasswordComplexity = 1", "PasswordComplexity = 0").replace("MinimumPasswordLength = 7", "MinimumPasswordLength = 1") | Out-File C:\secpol.cfg
+    secedit /configure /db c:\windows\security\local.sdb /cfg c:\secpol.cfg /areas SECURITYPOLICY
+    Remove-Item -force c:\secpol.cfg -confirm:$false
+}
+
+function BuffPasswordPolicy(){
+    secedit /export /cfg c:\secpol.cfg
+    (Get-Content C:\secpol.cfg).replace("PasswordComplexity = 0", "PasswordComplexity = 1").replace("MinimumPasswordLength = 1", "MinimumPasswordLength = 7") | Out-File C:\secpol.cfg
+    secedit /configure /db c:\windows\security\local.sdb /cfg c:\secpol.cfg /areas SECURITYPOLICY
+    Remove-Item -force c:\secpol.cfg -confirm:$false
 }
 
 $jsonData = Get-Content $Jsonfile | ConvertFrom-JSON
--- random_domain.ps1
+++ random_domain.ps1
@@ -1,11 +1,37 @@
-$group_names = (Get-Content "code/data/groups.txt")
-$first_names = (Get-Content "code/data/firstNames.txt")
-$last_names = (Get-Content "code/data/lastNames.txt")
-$passwords = (Get-Content "code/data/passwords.txt")
+$jsonData = Get-Content .\ad_config.json | ConvertFrom-JSON
+$Global:Domain = $jsonData.domain
+$num_groups = 12
+$MaxUserGroups = 2
+
+$group_names = (Get-Content "data/groups.txt")
+$first_names = (Get-Content "data/firstNames.txt")
+$last_names = (Get-Content "data/lastNames.txt")
+$passwords = (Get-Content "data/passwords.txt")
+$groups = @(Get-Random -InputObject $group_names -Count $num_groups)
+$users = @()
 
 
 
-$groups = @()
-$num_groups = 10
+#echo (Get-Random -InputObject $group_names -Count $num_groups)
 
-echo (Get-Random -InputObject $first_names -Count 5)
+$num_users = 10
+for ($i = 0; $i -lt $num_users; $i++) {
+    $first_name = (Get-Random -InputObject $first_names)
+    $last_name = (Get-Random -InputObject $last_names)
+    $password = (Get-Random -InputObject $passwords)
+    $new_user = @{
+        "name"="$first_name $last_name"
+        "password"="$password"
+        "groups"=@((Get-Random -InputObject $groups -Count (Get-Random -Minimum 1 -Maximum $MaxUserGroups)))
+    }
+    $users += $new_user
+}
+
+
+$TableJson = ConvertTo-Json -InputObject @{
+    "domain"=$Domain
+    "groups"=([string]$groups)
+    "users"=$users
+}
+
+echo ($TableJson)
